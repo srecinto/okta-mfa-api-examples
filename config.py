9393fdcf268dcdc042952bee116713874ee7411a
Shawn Recinto <shawn.recinto@gmail.com>
2019-05-28 21:46:19 +0000

    Added Multi SMS MFA support

--- config.py
+++ config.py
@@ -5,5 +5,17 @@ okta = {
     "oidc_client_id": "", # OIDC Application created in Okta client id
     "oidc_client_secret": "", # OIDC Application created in Okta client secret
     "redirect_uri": "", # OIDC Redirect URI to handle auth code flow
-    "post_oidc_redirect": "" # url to redirect to after successfuly authenticating and getting an oath token
+    "post_oidc_redirect": "", # url to redirect to after successfuly authenticating and getting an oath token
+    "multi_sms_primary_number": "", # Primary SMS number to always reset to for supporting multiple SMS numbers.  This variable is defined in Okta as a string
+    "multi_sms_allowed_numbers": "" # List of allowed SMS numbers.  This variable is defined in Okta as a string array
 }
+
+
+"""
+User -> Login
+Login -> promtps for factor enrolled_factors
+User -> selects phone
+User -> select Factor Type 
+
+
+"""
